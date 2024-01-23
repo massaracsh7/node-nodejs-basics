8e4f5aeea0b72eab9c8b5e6dc5dd03014cfe04d7
massaracsh7 <fatum7@gmail.com>
2024-01-23 22:35:20 +0300

    feat: add rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,37 @@
+import {
+  join,
+  dirname
+} from 'path';
+import fs from 'fs/promises';
+import {
+  fileURLToPath
+} from 'url';
+
 const rename = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(
+    import.meta.url));
+  const errorMsg = 'FS operation failed';
+  const oldFile = join(__dirname, 'files', 'wrongFilename.txt');
+  const newFile = join(__dirname, 'files', 'properFilename.md');
+  try {
+    await fs.access(newFile);
+    throw new Error(errorMsg);
+  } catch (error) {
+    if (error.message === errorMsg) {
+      console.error(error.message);
+      return;
+    }
+
+    try {
+      if (!oldFile) {
+        throw new Error(errorMsg);
+      }
+      await fs.rename(oldFile, newFile);
+      console.log('File renamed: wrongFilename.txt to properFilename.md');
+    } catch (error) {
+      console.error(errorMsg);
+    }
+  }
 };
 
 await rename();
