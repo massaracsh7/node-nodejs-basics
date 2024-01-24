af00191192440aed5c59d068a1cd12af1a83f54a
massaracsh7 <fatum7@gmail.com>
2024-01-24 21:43:31 +0300

    feat: add compress, decompress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
+import {
+  join,
+  dirname
+} from 'path';
+import {
+  createWriteStream, createReadStream
+} from 'fs';
+import {
+  fileURLToPath
+} from 'url';
+import { createGzip } from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(
+      import.meta.url);
+    const __dirname = dirname(__filename);
+    const file = join(__dirname, 'files', 'fileToCompress.txt');
+    const archive = join(__dirname, 'files', 'archive.gz');
+    const fileStream = createReadStream(file);
+    const archiveStream = createWriteStream(archive);
+    const toArchive = createGzip();
+    fileStream.pipe(toArchive).pipe(archiveStream);
+    console.log('Compresses file fileToCompress.txt to archive.gz');
 };
 
 await compress();
