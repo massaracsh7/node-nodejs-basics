832b5ef68d092894e37e0871d6c4a93b21b14f64
massaracsh7 <fatum7@gmail.com>
2024-01-24 11:52:04 +0300

    feat: add env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envObject = process.env;
+  const rssArray = Object.keys(envObject)
+    .filter(key => key.startsWith('RSS_'))
+    .reduce((arr, key) => {
+      arr.push(`${key}=${envObject[key]}`);
+      return arr;
+    }, []);
+  const str = rssArray.join('; ');
+  console.log(str);
 };
 
 parseEnv();
