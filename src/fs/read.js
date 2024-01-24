b02ab63ef1b2fdb0ae24f375081d3f97f45b224a
massaracsh7 <fatum7@gmail.com>
2024-01-24 10:58:29 +0300

    feat: add read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,23 @@
+import {
+  join,
+  dirname
+} from 'path';
+import fs from 'fs/promises';
+import {
+  fileURLToPath
+} from 'url';
+
 const read = async () => {
-    // Write your code here 
-};
+  const __dirname = dirname(fileURLToPath(
+    import.meta.url));
+  const errorMsg = 'FS operation failed';
+  const file = join(__dirname, 'files', 'fileToRead2.txt');
+  try {
+    const text = await fs.readFile(file, 'utf-8');
+    console.log(text);
+  } catch (error) {
+    throw new Error(errorMsg);
+  }
+  };
 
 await read();
