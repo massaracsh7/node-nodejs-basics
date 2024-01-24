6fcdc7daa91f3abd89d0da31c27a77eda5f76045
massaracsh7 <fatum7@gmail.com>
2024-01-24 15:19:03 +0300

    feat: add calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import {
+  join,
+  dirname
+} from 'path';
+import fs from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { createHash } from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(
+    import.meta.url));
+  const file = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+  const hash = createHash('sha256');
+  try {
+    const text = await fs.readFile(file);
+    const hashFile = hash.update(text).digest('hex');
+    console.log(hashFile);
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 await calculateHash();
