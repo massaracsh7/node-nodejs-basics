b0423bac1d56c176964fcbfa0919112caf3b33e5
massaracsh7 <fatum7@gmail.com>
2024-01-24 13:43:47 +0300

    feat: add args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const arr = args.reduce((acc, val, i) => {
+    if (i % 2 === 0 && val.startsWith('--')) {
+      const name = val.replace(/^--/, '');
+      acc.push(`${name} is ${args[i + 1]}`);
+    }
+    return acc;
+  }, []);
+    const str = arr.join(', ');
+    console.log(str);
 };
 
 parseArgs();
